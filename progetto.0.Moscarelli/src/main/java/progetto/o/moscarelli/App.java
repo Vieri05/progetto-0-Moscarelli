ee2233ea8cd23193f89083a5140952354ca65ce0
Vieri05 <128602456+Vieri05@users.noreply.github.com>
2023-09-18 11:29:42 +0200

    Add files via upload

---
+++ progetto.0.Moscarelli/src/main/java/progetto/o/moscarelli/App.java
@@ -0,0 +1,13 @@
+package progetto.o.moscarelli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
